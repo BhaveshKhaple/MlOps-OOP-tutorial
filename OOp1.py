66952425d8612f9bd33cd2482a859cac1d266b6b
BhaveshKhaple <122960021+BhaveshKhaple@users.noreply.github.com>
2025-04-23 23:54:27 +0530

    1st commit on OOP1py

---
+++ OOp1.py
@@ -0,0 +1,10 @@
+# initiate a class
+class employee:
+    #special method/magic/dunder - constructor
+    def __init__(self):
+        self.name="BHavesh"
+        self.id="123"
+        self.designation="DA"
+
+E1=employee()
+print(E1.name)
